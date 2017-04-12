6700542a7182eaddd60dd3b05f158733cc55be89
pkowalski <piet.kowalski@gmail.com>
2017-04-12 09:21:08 +0100

    Fix missing PK identifier for Car entity

--- src/AppBundle/Entity/Car.php
+++ src/AppBundle/Entity/Car.php
@@ -3,13 +3,18 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Table(name="cars")
- * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CarRepository")
  */
 class Car
 {
 
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
 }
