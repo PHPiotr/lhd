07f7c5b9b7fec116151d54c18a09c44593140586
pkowalski <piet.kowalski@gmail.com>
2017-04-16 01:56:27 +0100

    Display and style Part Exchange page

--- src/AppBundle/Controller/ExchangeController.php
+++ src/AppBundle/Controller/ExchangeController.php
@@ -5,6 +5,13 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ExchangeController extends Controller
 {
@@ -13,9 +20,43 @@ class ExchangeController extends Controller
      * @Route("/part-exchange", name="exchange")
      * @Template()
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return [];
+        $form = $this->createFormBuilder()
+            ->add('name', null, ['constraints' => [new NotBlank()], 'required' => true, 'attr' => ['heading' => 'About you']])
+            ->add('telephone_number', null, ['constraints' => [new NotBlank()], 'required' => true])
+            ->add('email_address', EmailType::class, ['constraints' => [new NotBlank()], 'required' => true])
+            ->add('reg_number', null, ['constraints' => [new NotBlank()], 'attr' => ['heading' => 'About your car']])
+            ->add('make', null, ['constraints' => [new NotBlank()], 'required' => true])
+            ->add('model', null, ['constraints' => [new NotBlank()], 'required' => true])
+            ->add('mileage', null, ['constraints' => [new NotBlank()], 'required' => true])
+            ->add('mot', null, ['constraints' => [new NotBlank()], 'required' => true])
+            ->add('fuel_type', null, ['constraints' => [new NotBlank()], 'required' => true])
+            ->add('transmission', null, ['constraints' => [new NotBlank()], 'required' => true])
+            ->add('colour', null, ['constraints' => [new NotBlank()], 'required' => true])
+            ->add('service_history', null, ['constraints' => [new NotBlank()], 'required' => true])
+            ->add('general_condition', null, ['constraints' => [new NotBlank()], 'required' => true])
+            ->add('defects', null, ['constraints' => [new NotBlank()], 'required' => true])
+            ->add('vehicle_interested_in', null, ['constraints' => [new NotBlank()], 'required' => true])
+            ->add('additional_notes', TextareaType::class)
+            ->add('send', SubmitType::class, ['attr' => ['class' => 'btn-danger']])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if (!$form->isSubmitted()) {
+            return ['form' => $form->createView()];
+        }
+
+        if (!$form->isValid()) {
+            foreach ($form->getErrors() as $error) {
+                $this->addFlash('danger', $error->getMessage());
+            }
+            return ['form' => $form->createView()];
+        }
+
+        $this->addFlash('success', 'Thanks, your message has been sent. We will contact you soon.');
+        return $this->redirectToRoute('exchange');
     }
 
 }
---
+++ src/AppBundle/Form/ExchangeType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Validator\Constraints\IsTrue;
+
+class ExchangeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('confirm', CheckboxType::class, array(
+                'label'    => 'Do you really want to delete this car from stock?',
+                'required' => true,
+                'constraints'=> new IsTrue(array('message'=>'Please confirm'))
+            ))
+            ->add('save', SubmitType::class);
+    }
+
+}
